dc9bb1ab6b43d570fa98ae889e32ebb778e6564e
Meryem <meryem.nobatova.1@iliauni.edu.ge>
2026-05-20 01:06:09 +0200

    feat: add AR assets/vuforia and linkhandler script

---
+++ Assets/LinkHandler.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class LinkHandler : MonoBehaviour
+{
+    public void OpenLinkedIn()
+    {
+        Application.OpenURL("https://www.linkedin.com/in/meryem-nobatova-095459410"); 
+    }
+
+    public void OpenGitHub()
+    {
+        Application.OpenURL("https://github.com/Meryemmmn");
+    }
+}
